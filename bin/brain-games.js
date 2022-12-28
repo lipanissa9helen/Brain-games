b1b317858520590a7404e22fcb5b2a2d606adbce
Sivkova Alena <lipanissa9@gmail.com>
2022-12-28 18:19:01 +0700

    Library connection readline-sync Implementing a username greeting

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,16 @@
 #!/usr/bin/env node
-console.log("Welcome to the Brain Games!") 
+
+import name from '../src/cli.js';
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/cli.js
@@ -0,0 +1,15 @@
+import readlineSync from 'readline-sync';
+
+
+console.log("Welcome to the Brain Games!")
+const name = readlineSync.question("May I have your name? ");
+console.log('Hello, ' + name + '!');
+
+export default name;
+
+
+
+
+
+
+
