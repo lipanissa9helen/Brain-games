56378538f838892b0cb34e6a51eeb1a67fce2ac7
Sivkova Alena <lipanissa9@gmail.com>
2023-01-19 19:56:28 +0700

    refactorimg brain-gcd

--- src/games/game-calc.js
+++ src/games/game-calc.js
@@ -9,13 +9,15 @@ function randomOperation() {
 }
 
 function calculator(number1, operation, number2) {
+  let result = 0;
   if (operation === '+') {
-    return number1 + number2;
+    result = number1 + number2;
   } if (operation === '-') {
-    return number1 - number2;
+    result = number1 - number2;
   } if (operation === '*') {
-    return number1 * number2;
+    result = number1 * number2;
   }
+  return result;
 }
 
 function gameEngine() {
